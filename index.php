89eeaacde2dc07377cd2d91bf87ce59c9e5a4ce0
Jeruzael <dumale.jeruzael.bscs2019@gmail.com>
2022-06-05 22:13:04 +0800

    up

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jeruzael</title>
+</head>
+<body>
+	<h2>PHP file</h2>
+</body>
+</html>
